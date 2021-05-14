194cd5a04cb12399557e4f94f1d9e7346723bb0f
alessiotoni <toniellialessio@gmail.com>
2021-05-14 12:10:08 +0200

    completa milestone 3 e 4 (temporanemante)

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ const app = new Vue({
             "movie", "tv",
         ],
         typeToSearch: "",
+        showSearchBar: false
     },
     computed: {
 
@@ -25,15 +26,17 @@ const app = new Vue({
             axios.get("https://api.themoviedb.org/3/search/movie", query)
                 .then((resp) => {
                     const moviesList = resp.data.results.map((movie) => {
-                        movie.type = "movie"
+
+                        movie.type = "movie";
+                        movie.img = movie.poster_path
+                        
                         return movie
                     })
                     moviesList.forEach((element) => {
+
                         this.moviesList.push(element);
                         this.globalList.push(element);
                     });
-                    
-
                 })
         },
         searchSeriesTv() {
@@ -47,19 +50,23 @@ const app = new Vue({
             axios.get("https://api.themoviedb.org/3/search/tv", query)
                 .then((resp) => {
                     const seriesTvList = resp.data.results.map((serieTv) => {
+
                         serieTv.title = serieTv.name
                         serieTv.original_title = serieTv.original_name
+                        serieTv.img = serieTv.poster_path
                         serieTv.type = "seriesTv"
+
                         return serieTv
                     })
                     seriesTvList.forEach((element) => {
+
                         this.seriesTvList.push(element);
                         this.globalList.push(element);
                     })
-
                 })
         },
         searchOnClick() {
+
             this.globalList = []
             this.seriesTvList = []
             this.moviesList = []
@@ -81,6 +88,7 @@ const app = new Vue({
             }
 
             this.textToSearch = ""
+            this.clickShowSearchBar()
         },
         getCountrysFlag(film) {
             if (film.original_language == 'en') {
@@ -88,12 +96,23 @@ const app = new Vue({
             } else {
                 return 'flag-icon-' + film.original_language
             }
-
-
-
+        },
+        getImg(film){
+            return "http://image.tmdb.org/t/p/w500" + film.img
+        },
+        voteAverageStars(film){
+            const voteAverage = Math.ceil(film.vote_average / 2)
+            const stars = []
+            for (let i = 0; i < voteAverage; i++){
+                stars.push(i)
+            }
+            return stars
+        },
+        clickShowSearchBar(){
+            this.showSearchBar = !this.showSearchBar
         }
     },
     mounted() {
-        // this.typeToSearch = "All"
+        
     },
 })
